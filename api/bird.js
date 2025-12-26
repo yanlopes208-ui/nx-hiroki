52bd8abe3f0c1b7588c67259052437ac4003286b
yanlopes208-ui <yanlopes208@gmail.com>
2025-12-26 20:06:19 -0300

    Update bird.js

--- api/bird.js
+++ api/bird.js
@@ -1,5 +1,5 @@
 export default function handler(req, res) {
-  const memes = [
+  const birds = [
     "https://cdn.discordapp.com/attachments/1393626963490373632/1412538809245106297/images_88.jpg?ex=68e42a47&is=68e2d8c7&hm=a7497d4c4a828d4911dde981b7b0c1fa77def4471c0a4d2709773a20ab72d4d1&",
     "https://cdn.discordapp.com/attachments/1393626963490373632/1412538809576591542/images_89.jpg?ex=68e42a47&is=68e2d8c7&hm=f56f6dd761097572fe4fe504dab09618a0a3cfda0e9ef450b530fb2f150fce2e&",
     "https://cdn.discordapp.com/attachments/1393626963490373632/1412538810142818476/images_90.jpg?ex=68e42a48&is=68e2d8c8&hm=3b0b0f76a7340bcf09e8c092551d5374af4074e13dcdfb6b6eefd16b0705d512&",
