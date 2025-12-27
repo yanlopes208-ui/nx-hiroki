4103efd4f41afd12ac9604ed6aece816ca9ecafd
yanlopes208-ui <yanlopes208@gmail.com>
2025-12-26 22:33:43 -0300

    Update cats.js

--- api/cats.js
+++ api/cats.js
@@ -1,1 +1,13 @@
 
+export default function handler(req, res) {
+  const birds = [
+
+      ];
+
+  const randomBird = birds[Math.floor(Math.random() * birds.length)];
+
+  res.status(200).json({
+    author: "Pássaros Random",
+    imagem: randomBird
+  });
+}
